e355414ef339e689c59dc16dfe73f6fb15049047
jannepetter <peterdev80@gmail.com>
2024-06-17 19:50:20 +0300

    exercise 11.9. More tests

--- e2e-tests/pokemon.spec.js
+++ e2e-tests/pokemon.spec.js
@@ -10,4 +10,16 @@ describe("Pokedex", () => {
       )
     ).toBeVisible();
   });
+  test("Pokemon image can be clicked and navigated to", async ({ page }) => {
+    await page.goto("http://localhost:8080/");
+    await expect(page.getByText("ivysaur")).toBeVisible();
+    await expect(
+      page.getByText(
+        "Pokémon and Pokémon character names are trademarks of Nintendo."
+      )
+    ).toBeVisible();
+    await page.getByText("ivysaur").click();
+    await expect(page.getByText("chlorophyll")).toBeVisible();
+    expect(page.url()).toBe("http://localhost:8080/pokemon/ivysaur");
+  });
 });

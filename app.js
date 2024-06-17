070c892914e0261b9da8c61348f1fb28f428ed15
jannepetter <peterdev80@gmail.com>
2024-06-18 00:05:55 +0300

    deploy broken version

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static("dist"));
 
 app.get("/health", (req, res) => {
+  if (true) throw "error...  ";
   res.status(200).send();
 });
 
 app.get("/version", (req, res) => {
-  res.status(200).json({ version: "1" });
+  res.status(200).json({ version: "2" });
 });
 
 app.listen(PORT, () => {
